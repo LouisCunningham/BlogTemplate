5e729c0a8a0a2fe5461169be0f3b60b2ff11ddfc
LouisCunningham <cunningtechgroup@outlook.com>
2024-06-02 13:15:31 -0400

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'RelaXx Gaming';
+export const SITE_DESCRIPTION = 'Tech & Anime Reviews, LoFi Gaming Tutorials';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
